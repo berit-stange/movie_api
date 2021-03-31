113f354591b5da01888a03c09597cef6ef29307d
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-03-31 20:16:21 +0200

    removed movie-data array + required mongoose and models + added request methods with mongoose logic

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express'); //imports express module locally
 const morgan = require('morgan'); //imports Morgan for logging
 const bodyParser = require('body-parser'); 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
+mongoose.connect('mongodb://localhost:27017/moviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const app = express(); // configure webs server + route HTTP requests and responses
 
@@ -8,178 +14,6 @@ app.use(morgan('common')); //invokes middleware function
 app.use(express.static('public')); //serving static files
 app.use(bodyParser.json());
 
-let movies = [
-      {
-        title: 'Adams Apples',
-        year: '2015',
-        genre: {
-          name: 'Black comedy',
-          description: ''
-        },
-        director: {
-          name: 'Anders Thomas Jensen',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'I care a lot',
-        year: '2020',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'Parasite',
-        year: '2019',
-        genre: {
-          name: 'Drama',
-          description: 'This is a very long text about the genre of Drama'
-        },
-        director: {
-          name: 'Bong Joon-ho',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'One Flew Over the Cuckoos Nest',
-        year: '1975',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'Truman Show',
-        year: '1998',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'What we do in the shadows',
-        year: '2014',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'A Man Called Ove',
-        year: '2015',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'Jojo Rabbit',
-        year: '2019',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'Her',
-        year: '2013',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      },
-      {
-        title: 'Drive',
-        year: '2011',
-        genre: {
-          name: 'x',
-          description: ''
-        },
-        director: {
-          name: 'x',
-          birthyear:'',
-          deathyear:'',
-          bio: '', 
-          work: ''
-        },
-        imgURL: '',
-        actors: ''
-      }
-  ];
 
 
 app.get('/', (req, res) => {
@@ -187,68 +21,173 @@ app.get('/', (req, res) => {
 });
 
 
-//Return a list of ALL movies
-//GET  	/movies
+//1. Return a list of ALL movies --- //GET /movies
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
 });
 
-//Return data about movie by title
-//GET /movies/[title]
-app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movies) => 
-  { return movies.title === req.params.title }));
+
+//2. Return data about movie by title --- //GET /movies/[title]
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-//Return data about a genre (description) by name
-//GET /movies/genres/[name]
-app.get('/movies/genres/:name', (req, res) => {
-  let genre = movies.find((genre) => { return genre.name === req.params.name});
-  res.json(movies.find((genre) => 
-  { return genre.description === req.params.description }));
+//3. Return data about a genre (description) by name --- //GET /movies/genres/[name]
+app.get('/movies/genres/:Name', (req, res) => {
+    Movies.findOne({ "Genre.Name": req.params.Name })
+    .then((movie) => {
+      res.json(movie.Genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  });
+
+//4. Return data about a director (bio, birth year, death year, work) by name --- //GET  /movies/[director]/[name]
+app.get('/movies/directors/:Name', (req, res) => {
+    Movies.findOne({ "Director.Name": req.params.Name })
+    .then((movie) => {
+      res.json(movie.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  });
+
+//Get all users --- only for internal use 
+app.get('/users', (req,res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error ' + err);
+  });
 });
 
-//Return data about a director (bio, birth year, death year, work) by name
-//GET  /movies/[director]/[name]
-app.get('/movies/directors/:name', (req, res) => {
-  res.json(director.find((director) => 
-  { return director.name === req.params.name }));
+//Get user by username  --- only for internal use
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-//Allow new users to register     
-//POST  /users
+//5. Allow new users to register --- //POST  /users
 app.post('/users', (req, res) => {
-  //to be added
-  res.send('JSON object containing user data that was just added')
+  Users.findOne({ Username: req.body.Username })//check if a user with the username provided by the client already exists
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else {
+      Users.create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) =>{res.status(201).json(user)})
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
 });
 
-//Allow users to update their user info (username)   
-//PUT /users/[username]
-app.put('/users/:username', (req, res) => {
-  //to be added
-  res.send('Successful PUT request returning JSON object containing user data that was just updated');
+//6. Allow users to update their user info (username) --- //PUT /users/[username]
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {$set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, //makes sure updated info is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-//Allow users to add a movie to their list of favorites    
-//POST /users/[username]/favorites
-app.post('/users/:username/favorites', (req, res) => {
-  //to be added
-  res.send('JSON object containing movie data that was just added');
+//7. Allow users to add a movie to their list of favorites --- //POST /users/[username]/favorites
+app.post('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-//Allow users to remove a movie from their list of favorites   	
-//DELETE	/users/[username]/favorites
-app.delete('/users/:username/favorites', (req, res) => {
-  //to be added
-  res.send('text message saying the movie was successfully deleted');
+//8. Allow users to remove a movie from their list of favorites --- //DELETE	/users/[username]/favorites
+app.delete('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+ (err, updatedFavs) => {
+   if (err) {
+     console.error(err);
+     res.status(500).send('Error: ' + err);
+   } else {
+     res.json(updatedFavs);
+   }
+ });
 });
 
-//Allow existing users to deregister    
-//DELETE	/users/[username]
-app.delete('/users/:username', (req, res) => {
-  //to be added
-  res.send('text message saying the account was successfully deleted');
-});
+//9. Allow existing users to deregister --- //DELETE	/users/[username]
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+  .then((user) => {
+    if (!user) {
+      res.status(400).send(req.params.Username + ' was not found.');
+    } else {
+      res.status(200).send(req.params.Username + ' was deleted.');
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+ });
 
 
 
